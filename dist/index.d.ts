fcc3424a1d23edb216f781108fb9982c4c309f54
Yoni Farin <farin99@gmail.com>
2017-01-15 22:40:12 +0200

    delete dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./sh-select.module";
-export * from "./sh-select-menu.component";
-export * from "./sh-select-menu.component";
--- dist/sh-select.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class ShSelectModule {
-    static forRoot(): ModuleWithProviders;
-}
